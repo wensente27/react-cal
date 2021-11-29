f1f87d5a12f8a85a724ba8485e9f96d729168af5
wensente27 <jahpxpanique@gmail.com>
2021-11-29 09:25:39 +0800

    nice

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import parse from 'date-fns/parse'
 import startOfWeek from 'date-fns/startOfWeek'
 import getDay from 'date-fns/getDay'
 import "react-big-calendar/lib/css/react-big-calendar.css"
-import "react-datepicker/dist/react-datepicke r.css"
+import "react-datepicker/dist/react-datepicker.css"
 import React, { useState } from 'react'
 import DatePicker from 'react-datepicker'
 
